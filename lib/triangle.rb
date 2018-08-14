bcc3048b8deed5c014b49132bb59669f9c082a28
Michael Roose <mjroose@gmail.com>
2018-08-14 00:24:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,6 +3,22 @@ class Triangle
     @sides = [s1, s2, s3]
   end
 
+  def kind
+    
+      
+  end
+
+  def valid?
+    valid = true
+    @sides.each do |side|
+      valid = false if side <= 0
+    end
+    valid = valid && @sides[0] + @sides[1] > @sides[2]
+    valid = valid && @sides[1] + @sides[2] > @sides[0]
+    valid = valid && @sides[0] + @sides[2] > @sides[1]
+    valid
+  end
+  
   class TriangleError < StandardError
 
   end
